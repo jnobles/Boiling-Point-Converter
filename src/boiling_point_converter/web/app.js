95df0b7aa0c207c2b42f38f8bdb22897cb602734
inforussle <123Jimmyn@gmail.com>
2026-08-10 17:26:34 -0400

    feat: initial web form setup

---
+++ src/boiling_point_converter/web/app.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("calculator")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const referencePressure = document.getElementById("reference-pressure").value;
+    const referenceTemperature = document.getElementById("reference-temperature").value;
+    const mode = form.elements["solver-mode"].value;
+    const atValue = document.getElementById("at-value").value;
+
+    console.log({
+        referencePressure,
+        referenceTemperature,
+        mode,
+        atValue,
+    });
+});
