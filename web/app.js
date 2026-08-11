9dc2a58a8339d91c5e16a0d7d4c4ddf4ff4e3649
inforussle <123Jimmyn@gmail.com>
2026-08-11 13:16:06 -0400

    feat: mpv of web calculator

--- web/app.js
+++ web/app.js
@@ -1,23 +1,57 @@
-const form = document.getElementById("calculator")
+let pyodideReady;
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
+async function initializePyodide() {
+    console.time("Pyodide initialization");
 
-    const referencePressure = document.getElementById("reference-pressure").value;
-    const referenceTemperature = document.getElementById("reference-temperature").value;
-    const mode = form.elements["solver-mode"].value;
-    const atValue = document.getElementById("at-value").value;
+    console.time("loadPyodide");
+    const pyodide = await loadPyodide();
+    console.timeEnd("loadPyodide");
 
-    console.log({
-        referencePressure,
-        referenceTemperature,
-        mode,
-        atValue,
+    console.time("load micropip");
+    await pyodide.loadPackage("micropip");
+    console.timeEnd("load micropip");
+
+    console.time("install calculator");
+    const micropip = pyodide.pyimport("micropip");
+    await micropip.install("./boiling_point_converter-1.0.1-py3-none-any.whl");
+    console.timeEnd("install calculator");
+
+    console.log("Pyodide ready");
+    console.timeEnd("Pyodide initialization")
+    return pyodide
+}
+
+async function main() {
+    const form = document.getElementById("calculator");
+    const calculateButton = document.getElementById("calculate");
+
+    pyodideReady = initializePyodide();
+    pyodideReady.then(() => {
+        calculateButton.disabled = false;
+        calculateButton.textContent = "Calculate";
     });
-});
 
-async function main(){
-        let pyodide = await loadPyodide();
-        console.log(pyodide.runPython("1 + 2"));
-      }
-      main();
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const pyodide = await pyodideReady;
+
+        const referencePressure = Number(document.getElementById("reference-pressure").value);
+        const referenceTemperature = Number(document.getElementById("reference-temperature").value);
+        const mode = form.elements["solver-mode"].value;
+        const atValue = Number(document.getElementById("at-value").value);
+
+        pyodide.globals.set("p1", referencePressure);
+        pyodide.globals.set("t1", referenceTemperature);
+        pyodide.globals.set("at_value", atValue);
+        pyodide.globals.set("mode", mode)
+        pyodide.globals.set("dh_vap", 40);
+
+        const result = pyodide.runPython(`
+            from boiling_point_converter.core import perform_calculation
+            perform_calculation(mode, p1, t1, at_value, dh_vap)
+        `);
+        console.log(result);
+    });
+}
+
+ main();
