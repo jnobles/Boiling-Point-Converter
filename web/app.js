a6e1a68c6ed683c435f12ae7665f345e2efdf5d6
inforussle <123Jimmyn@gmail.com>
2026-08-11 17:59:00 -0400

    feat: connect dhVap table to pyodide web worker

--- web/app.js
+++ web/app.js
@@ -1,57 +1,66 @@
-let pyodideReady;
+const worker = new Worker(
+    "./pyodide-worker.js",
+    {type: "module"},
+);
 
-async function initializePyodide() {
-    console.time("Pyodide initialization");
-
-    console.time("loadPyodide");
-    const pyodide = await loadPyodide();
-    console.timeEnd("loadPyodide");
-
-    console.time("load micropip");
-    await pyodide.loadPackage("micropip");
-    console.timeEnd("load micropip");
-
-    console.time("install calculator");
-    const micropip = pyodide.pyimport("micropip");
-    await micropip.install("./boiling_point_converter-1.0.1-py3-none-any.whl");
-    console.timeEnd("install calculator");
-
-    console.log("Pyodide ready");
-    console.timeEnd("Pyodide initialization")
-    return pyodide
-}
-
-async function main() {
-    const form = document.getElementById("calculator");
-    const calculateButton = document.getElementById("calculate");
-
-    pyodideReady = initializePyodide();
-    pyodideReady.then(() => {
+worker.addEventListener("message", (event) => {
+    if (event.data.type === "ready") {
+        const calculateButton = document.getElementById("calculate");
         calculateButton.disabled = false;
         calculateButton.textContent = "Calculate";
+    }
+    if (event.data.type === "result") {
+        const resultOutput = document.getElementById("result");
+        resultOutput.textContent = event.data.result;
+    }
+    if (event.data.type === "dataTable") {
+        const dataTable = JSON.parse(event.data.json);
+        const fieldset = document.getElementById("heat-of-vaporization");
+        for (const item of dataTable) {
+            const option = document.createElement("input");
+            option.id = `dh-vap-${item.compound.replaceAll(" ", "-")}`;
+            option.type = "radio";
+            option.name = "dh-vap";
+            option.value = item.dh_vap_kj_per_mol;
+
+            const label = document.createElement("label");
+            label.htmlFor = option.id;
+            label.textContent = item.compound;
+
+            fieldset.appendChild(option);
+            fieldset.appendChild(label);
+        }
+    }
+});
+
+const form = document.getElementById("calculator");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const referencePressure = Number(document.getElementById("reference-pressure").value);
+    const referenceTemperature = Number(document.getElementById("reference-temperature").value);
+    const mode = form.elements["solver-mode"].value;
+    const atValue = Number(document.getElementById("at-value").value);
+    const dhVap = Number(document.getElementById("dh-vap").value);
+
+    worker.postMessage({
+        type: "calculate",
+        p1: referencePressure,
+        t1: referenceTemperature,
+        mode: mode,
+        atValue: atValue,
+        dhVap: dhVap,
     });
+});
 
-    form.addEventListener("submit", async (event) => {
-        event.preventDefault();
-        const pyodide = await pyodideReady;
+const fieldset = document.getElementById("heat-of-vaporization");
+const dhVap = document.getElementById("dh-vap");
 
-        const referencePressure = Number(document.getElementById("reference-pressure").value);
-        const referenceTemperature = Number(document.getElementById("reference-temperature").value);
-        const mode = form.elements["solver-mode"].value;
-        const atValue = Number(document.getElementById("at-value").value);
-
-        pyodide.globals.set("p1", referencePressure);
-        pyodide.globals.set("t1", referenceTemperature);
-        pyodide.globals.set("at_value", atValue);
-        pyodide.globals.set("mode", mode)
-        pyodide.globals.set("dh_vap", 40);
-
-        const result = pyodide.runPython(`
-            from boiling_point_converter.core import perform_calculation
-            perform_calculation(mode, p1, t1, at_value, dh_vap)
-        `);
-        console.log(result);
-    });
-}
-
- main();
+fieldset.addEventListener("change", (event) => {
+    if (event.target.type === "radio") {
+        dhVap.value = event.target.value === "custom"
+            ? ""
+            : event.target.value
+    }
+});
---
+++ web/pyodide-worker.js
@@ -0,0 +1,63 @@
+import {
+    loadPyodide
+} from "https://cdn.jsdelivr.net/pyodide/v314.0.4/full/pyodide.mjs"
+
+let pyodide;
+
+async function initializePyodide() {
+    pyodide = await loadPyodide();
+
+    await pyodide.loadPackage("micropip");
+    const micropip = pyodide.pyimport("micropip");
+    await micropip.install("./boiling_point_converter-1.0.1-py3-none-any.whl");
+    pyodide.runPython(`from boiling_point_converter.core import perform_calculation`)
+    console.log("Pyodide ready");
+    postMessage({
+        type: "ready",
+    });
+}
+
+async function heats_of_vaporization() {
+    const heats_of_vaporization = pyodide.runPython(`
+        import json
+        from dataclasses import asdict
+        from boiling_point_converter.core.molar_heat_of_vaporization import REFERENCE_HEATS_OF_VAPORIZATION as data_table
+        json.dumps([asdict(item) for item in data_table])
+    `);
+    postMessage({
+        type: "dataTable",
+        json: heats_of_vaporization,
+    });
+}
+
+async function calculate(data) {
+    pyodide.globals.set("p1", data.p1);
+    pyodide.globals.set("t1", data.t1);
+    pyodide.globals.set("mode", data.mode);
+    pyodide.globals.set("at_value", data.atValue);
+    pyodide.globals.set("dh_vap", data.dhVap);
+
+    const result = pyodide.runPython(`
+        perform_calculation(
+            mode=mode,
+            p1=p1,
+            t1=t1,
+            at_value=at_value,
+            dh_vap=dh_vap,
+        )
+    `);
+
+    postMessage({
+        type: "result",
+        result: result,
+    })
+}
+
+self.addEventListener("message", (event) => {
+    if (event.data.type === "calculate") {
+        calculate(event.data);
+    }
+});
+
+await initializePyodide();
+heats_of_vaporization();
